76cebba5d2f91f96dc739cb2aea79be58348d4b3
Михайленко Андрей Николаевич <mikhailenko.a@medindex.ru>
2026-05-14 11:35:56 +0300

    настройка terraform скрипта

--- Task1Advanced/envs/dev/backend.tf
+++ Task1Advanced/envs/dev/backend.tf
@@ -1,11 +1,20 @@
 terraform {
   backend "s3" {
-    endpoint   = "https://storage.yandexcloud.net"
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
     bucket     = "mihailenko.a1-backet"
     region     = "ru-central1"
     key        = "dev/vm.tfstate"
-
+    
+    # Обязательные параметры для Yandex S3
     skip_region_validation      = true
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_s3_checksum            = true
+    
+    use_path_style              = true
+    # disable_compute_service_status = true
   }
 }
